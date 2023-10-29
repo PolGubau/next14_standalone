940a70725733bb3a3cb0cf1f451814875e22aa8f
Pol Gubau Amores <gubaupol@gmail.com>
2023-10-29 15:47:10 +0100

    Pol Gubau; MW-Metadata options

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { lusitana } from "@/app/ui/fonts";
+import { Metadata } from "next";
 import Image from "next/image";
 
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
